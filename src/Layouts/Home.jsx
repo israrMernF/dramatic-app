380a2171b9bd12d9a1e67d2753161ec18cd8503f
israrMernF <israrbokthchowdhury4@gmail.com>
2025-11-09 23:40:02 +0600

    Footer

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -4,6 +4,7 @@ import ImageCarousel from '../Components/ImageCarousel'
 import MovieGallary from '../Components/MovieGallary'
 import { Toaster } from 'react-hot-toast'
 import MovieMustGallary from '../Components/MovieMustGallary'
+import Footer from '../Components/Footer'
 
 export default function Home() {
   const [searchText, setSearchText] = useState("")
@@ -12,11 +13,13 @@ export default function Home() {
       <Navbar searchText={searchText} setSearchText={setSearchText}></Navbar>
       <ImageCarousel></ImageCarousel>
 
+
       <main>
         <MovieGallary></MovieGallary>
         <Toaster position='top-right'></Toaster>
         <MovieMustGallary searchText= {searchText}></MovieMustGallary>
       </main>
+      <Footer></Footer>
     </div>
   )
 }
